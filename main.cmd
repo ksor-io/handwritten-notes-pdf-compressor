3bc26183bbd036463fe0610dec8c5c6bcf4d587a
Ksor <138730131+ksor-io@users.noreply.github.com>
2024-06-26 10:19:03 +0300

    Handle spaces

--- main.cmd
+++ main.cmd
@@ -5,14 +5,14 @@ where /Q gswin64c
 IF errorlevel 1 (
   echo ghostscript is not installed on your system. Go to https://ghostscript.com/releases/gsdnld.html, download and install. You may have forgotten to install ImageMagick ^(https://imagemagick.org/script/download.php^) too.
   PAUSE
-  EXIT
+  EXIT /B
 )
 
 where /Q magick.exe
 IF errorlevel 1 (
   echo ImageMagick is not installed on your system. Go to ^(https://imagemagick.org/script/download.php^), download and install.
   PAUSE
-  EXIT
+  EXIT /B
 )
 
 SET "installDir=%USERPROFILE%\AppData\Roaming\NotesCompressor"
@@ -27,10 +27,9 @@ IF NOT [%1] == [] (
   IF errorlevel 1 (
     echo Please run as admin ^(visit https://github.com/ksor-io/handwritten-notes-pdf-compressor for help.^)
     PAUSE
-    EXIT
+    EXIT /B
   )
-  echo Installing.
-  ::Made by https://github.com/ksor-io with <3.
+  echo Installing. Made by https://github.com/ksor-io with ^<3.
   IF EXIST "%settingsFile%" (
     echo A settings file has been detected. Would you like to keep the current settings ^(y^) or reset them to defaults ^(n^)? 
     SET /p "to_reset=[y/n]: "
@@ -38,7 +37,7 @@ IF NOT [%1] == [] (
     IF /I "!to_reset!"=="no" GOTO dont_backup
 
     SET "restore_settings=True"
-    move "%settingsFile%" "%USERPROFILE%\AppData\Local\Temp\NotesCompressor\" >NUL 2>&1
+    move "%settingsFile%" "%USERPROFILE%\AppData\Local\Temp\" >NUL 2>&1
     echo Noted, I will make sure to keep your settings.
     GOTO exit_if
 
@@ -58,19 +57,17 @@ IF NOT [%1] == [] (
   )
   IF DEFINED restore_settings (
     echo Restoring settings...
-    move "%USERPROFILE%\AppData\Local\Temp\NotesCompressor\settings.txt" "%installDir%\" >NUL 2>&1
-    rmdir /Q /s "%USERPROFILE%\AppData\Local\Temp\NotesCompressor"
+    move "%USERPROFILE%\AppData\Local\Temp\settings.txt" "%installDir%\" >NUL 2>&1
   ) ELSE (
     echo Resetting settings...
     CALL :create_settings
   )
   echo Install is done. You may delete this file ^(%0^) if you wish.
   PAUSE
-  EXIT
+  EXIT /B
 )
 
 :load_settings
-
 IF EXIST "%settingsFile%" (
   FOR /F %%i IN (%settingsFile%) DO SET %%i
 ) ELSE (
@@ -98,24 +95,24 @@ SET /a "thresholdInBytes=%threshold%*%multiplier%"
 
 SET "fileSize=%~z1"
 
-IF "%fileSize%" LEQ "%thresholdInBytes%" (
+IF %fileSize% LEQ %thresholdInBytes% (
   
   ::file is already small enough, so we can compress losslessly with ghostscript
   echo Compressing...
-  gswin64c.exe -q -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=%1\..\%~n1-compreesed.pdf %1 -dBATCH
+  gswin64c.exe -q -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile="%~p1\%~n1-compressed.pdf" %1 -dBATCH
   
   ::only way to check a file's size I could find is to load it in like so
-  echo %1\..\%~n1-compreesed.pdf > %tempFolder%\compreesedFileLocation.txt
-  FOR /F %%i IN (%tempFolder%\compreesedFileLocation.txt) DO (
+  echo %~p1\%~n1-compressed.pdf > "%tempFolder%\compressedFileLocation.txt"
+  FOR /F %%i IN ("%tempFolder%\compressedFileLocation.txt") DO (
     IF %fileSize% LSS %%~zi (
       echo NOTE: The file you are trying to compress is already smaller than the threshold. We did not manage to reduce it even further without loss of quality.
-      rmdir /Q /s %tempFolder%
-      del %1\..\%~n1-compreesed.pdf
+      rmdir /Q /s "%tempFolder%"
+      del "%~p1\%~n1-compressed.pdf"
       PAUSE
-      EXIT
+      EXIT /B
     )
-    rmdir /Q /s %tempFolder%
-    EXIT
+    rmdir /Q /s "%tempFolder%"
+    EXIT /B
   )
 )
 
@@ -124,23 +121,23 @@ IF "%fileSize%" LEQ "%thresholdInBytes%" (
 SET "DPI=450"
 SET "Q=90"
 
-mkdir "%tempFolder%\temp-images"
+mkdir "%tempFolder%\temp-images" >NUL 2>&1
 :return_here_if_too_big
 
 :: convert PDF to images
 echo Converting to JPEGs...
-gswin64c.exe -q -dNOPAUSE -sDEVICE=jpeg -r%DPI% -dJPEGQ=%Q% -sOutputFile="%tempFolder%\temp-images\page-%%04d.jpeg" "%1" -dBATCH
+gswin64c.exe -q -dNOPAUSE -sDEVICE=jpeg -r%DPI% -dJPEGQ=%Q% -sOutputFile="%tempFolder%\temp-images\page-%%04d.jpeg" %1 -dBATCH
 
 :: convert back
 echo Converting back to PDF...
-magick "%tempFolder%\temp-images\page-*.jpeg" "%1\..\%~n1-compreesed.pdf"
+magick "%tempFolder%\temp-images\page-*.jpeg" "%~p1\%~n1-compressed.pdf"
 
-echo "%1\..\%~n1-compreesed.pdf" > "%tempFolder%\compreesedFileLocation.txt"
-FOR /F %%i IN ("%tempFolder%\compreesedFileLocation.txt") DO (
+echo %~p1\%~n1-compressed.pdf > "%tempFolder%\compressedFileLocation.txt"
+FOR /F %%i IN ("%tempFolder%\compressedFileLocation.txt") DO (
   IF %%~zi LSS %thresholdInBytes% (
-    echo File compreesed.
+    echo File compressed.
     rmdir /Q /s "%tempFolder%"
-    EXIT
+    EXIT /B
   )
   ELSE (
     IF %Q% GTR 5 (
@@ -152,7 +149,7 @@ FOR /F %%i IN ("%tempFolder%\compreesedFileLocation.txt") DO (
     echo This file is too big... we were unable to compress it below the threshold.
     rmdir /Q /s "%tempFolder%"
     PAUSE
-    EXIT
+    EXIT /B
   )
 )
 
